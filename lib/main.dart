fdc66e6309bd98e5ca96c550552fa8c77cff89ca
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-02-19 16:40:23 +0200

    use media query

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,29 @@ class MediaQueryTest extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    //by using media query, you query the media(screen) of the device and save it in var to use it
+    var height = MediaQuery.of(context).size.height;
+    return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text('Hallo world'),),
+        body: Column(
+          children: [
+            Container(
+              height: height * 0.15,
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: height * 0.15,
+              width: 200,
+              color: Colors.black,
+            ),
+            Container(
+              height: height * 0.15,
+              width: 200,
+              color: Colors.yellow,
+            ),
+          ],
+        ),
       ),
     );
   }
