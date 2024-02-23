69f91febb1a83950b5fc2ac3fa556f7e3f7271cb
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-02-23 16:42:12 +0200

    use layout builder to achieve adaptive UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,114 @@ class MediaQueryTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //by using media query, you query the media(screen) of the device and save it in var to use it
-    var height = MediaQuery.of(context).size.height;
+    // var height = MediaQuery.of(context).size.height;
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: height * 0.15,
-              width: 200,
-              color: Colors.red,
+        //use LayoutBuilder to query which platform is being used
+        body: LayoutBuilder(builder: (context, constraints) {
+          if (constraints.maxWidth <= 500) {
+            return const MobileLayout();
+          } else {
+            return const DesktopLayout();
+          }
+        }),
+      ),
+    );
+  }
+}
+
+class DesktopLayout extends StatefulWidget {
+  const DesktopLayout({super.key});
+
+  @override
+  State<DesktopLayout> createState() => _DesktopLayoutState();
+}
+
+class _DesktopLayoutState extends State<DesktopLayout> {
+  int x = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () {
+                x = index;
+                setState(() {});
+              },
+              child: Container(
+                color: Colors.yellow,
+                margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+                child: Text(
+                  '${index + 1}',
+                  style: const TextStyle(fontSize: 36),
+                ),
+              ),
             ),
-            Container(
-              height: height * 0.15,
-              width: 200,
-              color: Colors.black,
+          ),
+        ),
+        Expanded(
+            child: Center(
+          child: Container(
+            color: Colors.red,
+            child: Text(
+              '${x + 1}',
+              style: const TextStyle(fontSize: 32),
             ),
-            Container(
-              height: height * 0.15,
-              width: 200,
-              color: Colors.yellow,
-            ),
-          ],
+          ),
+        ))
+      ],
+    );
+  }
+}
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => DetailsView(index: index)));
+        },
+        child: Container(
+          color: Colors.yellow,
+          margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+          child: Text(
+            '${index + 1}',
+            style: const TextStyle(fontSize: 36),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsView extends StatelessWidget {
+  const DetailsView({super.key, required this.index});
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(
+          '${index + 1}',
+          style: const TextStyle(
+            fontSize: 32,
+          ),
         ),
       ),
     );
