e4e891c4e826b0ad88366cf279b97795be56c133
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-03-03 16:13:15 +0200

    add drawer widget

--- lib/views/home_views.dart
+++ lib/views/home_views.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_ui/views/custom_drawer.dart';
 import 'package:responsive_ui/views/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer: const CustomDrawer(),
       backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
-        leading: const Icon(
-          Icons.menu,
-          color: Colors.white,
+        leading: GestureDetector(
+          onTap: (){
+            scaffoldKey.currentState!.openDrawer();
+          },
+          child: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
         ),
         backgroundColor: Colors.black,
       ),
