94a0d4eec41706fec137314d2d5c7de8ef7bb175
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-03-05 15:26:34 +0200

    hide appBar in desktop mode

--- lib/views/home_views.dart
+++ lib/views/home_views.dart
@@ -18,19 +18,57 @@ class _HomeViewState extends State<HomeView> {
       key: scaffoldKey,
       drawer: const CustomDrawer(),
       backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(
-        leading: GestureDetector(
-          onTap: (){
-            scaffoldKey.currentState!.openDrawer();
-          },
-          child: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Colors.black,
-      ),
+      appBar: buildAppBar(context),
       body: const HomeViewBody(),
     );
   }
+
+  AppBar? buildAppBar(BuildContext context) {
+    //-32 because of padding widget in layoutBuilder which layoutBuilder give max
+    //width which its parent gives
+    return MediaQuery.of(context).size.width -32 < 900
+        ? AppBar(
+            leading: GestureDetector(
+              onTap: () {
+                // scaffoldKey.currentState!.openDrawer();
+              },
+              child: const Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+            ),
+            backgroundColor: Colors.black,
+          )
+        : null;
+  }
 }
+
+// class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+//   const CustomAppbar({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(builder: (context, constrains) {
+//       if (constrains.maxWidth < 900) {
+//         return AppBar(
+//           leading: GestureDetector(
+//             onTap: () {
+//               // scaffoldKey.currentState!.openDrawer();
+//             },
+//             child: const Icon(
+//               Icons.menu,
+//               color: Colors.white,
+//             ),
+//           ),
+//           backgroundColor: Colors.black,
+//         );
+//       } else {
+//         return const SizedBox();
+//       }
+//     });
+//   }
+//
+//   @override
+//   // TODO: implement preferredSize
+//   Size get preferredSize => const Size.fromHeight(58);
+// }
